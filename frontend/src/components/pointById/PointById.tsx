c4ad69cf4ede426bc622f877ea86c769ae5c3a9d
Admin <admin@MacBook-Pro-Admin.local>
2020-08-23 21:54:01 +0200

    added icons

--- frontend/src/components/pointById/PointById.tsx
+++ frontend/src/components/pointById/PointById.tsx
@@ -3,6 +3,9 @@ import { useEffect } from "react";
 import { useParams } from "react-router";
 import { useHttpClient } from "../../hooks/useHttpClient";
 import { Maps, ErrorAlert, LoadingBar } from "../../components/";
+import { IconContext } from "react-icons/lib";
+import { MdAssistantPhoto } from "react-icons/md";
+import { AiOutlineCaretDown, AiOutlineCaretLeft, AiOutlineCaretRight, AiOutlineCaretUp } from "react-icons/ai";
 
 export interface LatLng {
   lat: number;
@@ -27,7 +30,7 @@ export const PointById: FunctionComponent = (): ReactElement | null => {
         const responseData = await sendRequest(
           `http://localhost:${process.env.REACT_APP_BACKEND_PORT}/${pointId}`
         );
-        setMapData(responseData);
+        setMapData(responseData.point);
       } catch (err) {
         console.warn("cannot find point", err.message);
       }
@@ -46,7 +49,24 @@ export const PointById: FunctionComponent = (): ReactElement | null => {
   if (mapData) {
     return (
       <div style={{ width: '100%', height: '300px' }}>
-        <p>{mapData.address}</p>
+        <p>Address: {mapData.address}</p>
+        <p>Flag: {mapData.flag.map((color: string, i: number) => {
+          return (
+            <IconContext.Provider
+              value={{ color, className: "global-class-name", size: "1.5em" }} key={i}
+            >
+              <MdAssistantPhoto />
+            </IconContext.Provider>
+          )})}</p>
+          <p>Direction: {
+    
+              <IconContext.Provider
+                value={{ className: "global-class-name", size: "1.5em" }}
+              >
+                {mapData.direction === "top" ? <AiOutlineCaretUp /> : mapData.direction === "down" ? <AiOutlineCaretDown/> : mapData.direction === "left"  ? <AiOutlineCaretLeft/> : <AiOutlineCaretRight/> }
+              </IconContext.Provider>
+          }</p>
+
         <Maps locations={[mapData]} />
       </div>
     );
